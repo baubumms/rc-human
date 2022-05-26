4113b018b5733e2f5a804f5e4c26f54084e82c3d
leolurch <40560457+leolurch@users.noreply.github.com>
2022-05-26 22:36:00 +0200

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,9 @@
 #include <Servo.h>
 #include "hoverhack.h"
 
+#define HEAD_SERVO_PIN D1
+#define FIRE_SERVO_PIN D3
+
 const char *websockets_server_host = "vps.leolurch.de"; // Enter server adress
 const uint16_t websockets_server_port = 8081;           // Enter server port
 
@@ -111,32 +114,20 @@ void setup()
 {
   Serial.begin(9600);
 
-  headServo.attach(D1);
-  fireServo.attach(D3);
+  headServo.attach(HEAD_SERVO_PIN);
+  fireServo.attach(FIRE_SERVO_PIN);
   fireServo.writeMicroseconds(fireServoMidpoint);
 
   // WiFiManager
   // Local intialization. Once its business is done, there is no need to keep it around
   WiFiManager wifiManager;
-  // reset saved settings
   // wifiManager.resetSettings();
 
   // set custom ip for portal
   // wifiManager.setAPStaticIPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
 
-  // fetches ssid and pass from eeprom and tries to connect
-  // if it does not connect it starts an access point with the specified name
-  // here  "AutoConnectAP"
-  // and goes into a blocking loop awaiting configuration
-  wifiManager.autoConnect("AutoConnectAP");
-  // or use this for auto generated name ESP + ChipID
-  // wifiManager.autoConnect();
+  wifiManager.autoConnect("RC-Human-AP");
 
-  // if you get here you have connected to the WiFi
-
-  // setting up the web socket client now
-
-  // run callback when messages are received
   client.onMessage(onMessageCallback);
 
   // run callback when events are occuring
